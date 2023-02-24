2fcd65f9110af0c2f8ee912915b7bf4219be61a5
Maxence Leroy <maxencel@bam.tech>
2023-02-24 17:10:23 +0100

    game over and restart ui

--- level/base_level.gd
+++ level/base_level.gd
@@ -3,8 +3,10 @@ extends Node2D
 class_name BaseLevel
 
 var player: Player = null
+onready var game_over_container: CenterContainer = $CanvasLayer/GameOverContainer
 
 func _ready():
+	game_over_container.hide()
 	player = find_node("Player", true, false)
 	if player != null:
 		player.connect("moved_to_place", self, "_on_player_moved_to_new_place")
@@ -17,4 +19,8 @@ func _on_player_moved_to_new_place(new_place: Area2D):
 	place.add_player(player)
 
 func _on_player_died():
-	pass
+	game_over_container.show()
+
+
+func _on_restart_pressed():
+	get_tree().reload_current_scene()
