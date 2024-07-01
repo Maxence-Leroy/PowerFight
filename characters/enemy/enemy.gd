79bd763bb3db6f4aded9802e2e393a4c9d9a4b3f
Maxence Leroy <maxence.leroy3@gmail.com>
2024-07-01 12:30:08 +0200

    orc idle animation

--- characters/enemy/enemy.gd
+++ characters/enemy/enemy.gd
@@ -1,3 +1,16 @@
 extends Character
 
 class_name Enemy
+
+@onready var animation = $AnimatedSprite2D
+
+var rng = RandomNumberGenerator.new()
+var type = ""
+
+func _ready():
+	var type_id = rng.randi_range(1, 1)
+	if type_id == 1:
+		type="orc"
+	var time_to_wait_ms = rng.randi_range(0, 1000) / 1000.0
+	await get_tree().create_timer(time_to_wait_ms).timeout
+	animation.play(type + "_idle")
